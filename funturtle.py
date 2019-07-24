075b682f89397da066aa9750974c20cf6247631e
asaad21-meet <asaad21@meet.mit.edu>
2019-07-24 07:35:08 -0400

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,23 @@
+
+"""
+import turtle
+turtle.shape('turtle')
+fin=turtle.clone()
+fin.shape('square')
+fin.goto(100,100)
+fin.goto(100,0)
+
+fin.goto(00,0)
+fin.goto(0,100)
+fin.goto(100,100)
+t=turtle.clone()
+t.shape('triangle')
+fin.goto(300,300)
+fin.stamp()
+fin.goto(100,100)
+turtle.stamp()
+turtle.goto(0,-300)
+turtle.clearstamps()
+"""
+import turtle
+a=
